51506eb04ecd2a1c9035f2a96c3f7299dcdd4ecb
Govind Kumar <gk4051668@gmail.com>
2024-03-29 20:34:18 +0530

    added playwright

--- loaders.js
+++ loaders.js
@@ -1,11 +1,11 @@
-import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+import { PlaywrightWebBaseLoader } from "langchain/document_loaders/web/playwright";
 import { GithubRepoLoader } from "langchain/document_loaders/web/github";
 import { NotionAPILoader } from "langchain/document_loaders/web/notionapi";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 export const webLoader = async (website) => {
   if (!website) return false;
-  const loader = new PuppeteerWebBaseLoader(website);
+  const loader = new PlaywrightWebBaseLoader(website);
   const splitter = RecursiveCharacterTextSplitter.fromLanguage("html", {
     chunkSize: 1000,
     chunkOverlap: 100,
