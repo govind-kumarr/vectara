f7eb57aeb50ddbe10f2dd16e24887df4b6658695
Govind Kumar <gk4051668@gmail.com>
2024-03-29 19:56:31 +0530

    added home route

--- app.js
+++ app.js
@@ -72,6 +72,10 @@ app.post("/load/notion", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  console.log(`App is running on port: ${PORT}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
